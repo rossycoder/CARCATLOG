e3a96f9172fafa37c5bdc42eac3c4c7e8d3b64e5
rossycoder <rossy4586879@gmail.com>
2026-01-28 17:31:58 +0500

    now the fix

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,15 @@ export default defineConfig({
       }
     }
   },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
